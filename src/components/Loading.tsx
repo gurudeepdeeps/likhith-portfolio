b97ec7551ae88dd297f65e4288bf8192bf2e98ee
Gurudeep V <gurudeepv55@gmail.com>
2026-03-27 01:30:56 +0530

    Migrate from gsap-trial to free standard gsap package

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
+            <span> A Creative Video Editor</span> <span>A Creative Content Creator</span>
+            <span> A Creative Video Editor</span> <span>A Creative Content Creator</span>
           </Marquee>
         </div>
         <div
